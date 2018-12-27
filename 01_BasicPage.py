525e4549d28e3d997548cb45d3b7c718d5ff6ec3
Ketan P <kpimparkar@gmail.com>
2018-12-26 23:09:43 -0700

    Adding multiple routes to home page

--- 01_BasicPage.py
+++ 01_BasicPage.py
@@ -2,8 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def testFlask():
 	return "<h1><center>Hello Flask !!</center></h1>"
+	
+@app.route("/aboutus")
+def aboutus():
+	return """<h1><center>About us</center></h1>
+			  <p><center>This is a basic web app created using flask framework</center></p>"""
 	
 if __name__ == "__main__":
 	app.run(debug=True)
