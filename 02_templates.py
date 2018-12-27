131f77d3c4e2b6834ffe90d31bcd46c336a8b790
Ketan P <kpimparkar@gmail.com>
2018-12-27 11:10:55 -0700

    Using Flask templates and JINJA2 template engine to render the micro blog webapp

---
+++ 02_templates.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+## Dummy data to show on home page
+oldBlogs =[
+			{'author': 'Author self',
+			 'date'  : '27 Dec 2018',
+			 'Topic' : 'Introduction',
+			 'Text'  : 'Introduction to oneself is a great medium to self discovery.'
+			},
+			{'author': 'Author followed 1',
+			 'date'  : '27 Dec 2018',
+			 'Topic' : 'Daily',
+			 'Text'  : 'Wake up, dress up and show up !'
+			},
+			{'author': 'Author followed 2',
+			 'date'  : '27 Dec 2018',
+			 'Topic' : 'Goals',
+			 'Text'  : 'Efforts without a goal is similar to wandering aimlessly. You walk a lot but don\'t go anywhere.'
+			}
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template("home.html",blogStream=oldBlogs)
+
+@app.route('/about')
+def about():
+	return render_template("about.html", blogCount=len(oldBlogs))
+
+if __name__ == "__main__":
+	app.run(debug=True)
