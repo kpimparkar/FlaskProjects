8e1deea5f7f742186f74f98ebe9929ede4c2af28
Ketan P <kpimparkar@gmail.com>
2018-12-26 22:58:45 -0700

    Flask basic web app script with inbuilt debug mode run of the flask web server.

--- 01_BasicPage.py
+++ 01_BasicPage.py
@@ -3,4 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def testFlask():
-	return "Hello Flask !!"
+	return "<h1><center>Hello Flask !!</center></h1>"
+	
+if __name__ == "__main__":
+	app.run(debug=True)
